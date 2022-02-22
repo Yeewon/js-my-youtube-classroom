40e3576e61f3cd945425d19a448c916f176d10ed
Yeewon <yes3427@kookmin.ac.kr>
2022-02-23 00:00:44 +0900

    refactor: 검색 결과 구조 분해 할당 적용

--- src/js/templates/searchedVideoList.js
+++ src/js/templates/searchedVideoList.js
@@ -13,18 +13,20 @@ export const createSearchedVideoList = (videos) => {
 };
 
 export const searchedVideoListTemplate = ({ id, snippet }) => {
-  const date = formatDate(snippet.publishTime);
+  const { channelId, channelTitle, publishTime, title } = snippet;
+  const { videoId } = id;
+  const date = formatDate(publishTime);
   const buttonTemplate = saveButtonConroller(id.videoId);
 
-  return `<article class="clip js-video relative" data-video-id="${id.videoId}" data-title="${snippet.title}" data-channel-id="${snippet.channelId}" data-channel-title="${snippet.channelTitle}" data-publish-time="${snippet.publishTime}">
+  return `<article class="clip js-video relative" data-video-id="${videoId}" data-title="${title}" data-channel-id="${channelId}" data-channel-title="${channelTitle}" data-publish-time="${publishTime}">
                 <div class="preview-container">
-                   <iframe class="js-preview" width="100%" height="118" src="https://www.youtube.com/embed/${id.videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+                   <iframe class="js-preview" width="100%" height="118" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
                 </div>
                 <div class="content-container pt-2 px-1">
-                    <h3>${snippet.title}</h3>
+                    <h3>${title}</h3>
                     <div>
-                        <a href="https://www.youtube.com/channel/${snippet.channelId}" target="_blank" class="channel-name mt-1">
-                        ${snippet.channelTitle}
+                        <a href="https://www.youtube.com/channel/${channelId}" target="_blank" class="channel-name mt-1">
+                        ${channelTitle}
                         </a>
                         <div class="meta">
                            <p>${date}</p>
