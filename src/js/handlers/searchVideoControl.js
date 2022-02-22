26dfec4c0ffbda8da180e545ce21cd04385c5918
Yeewon <yes3427@kookmin.ac.kr>
2022-02-22 20:49:51 +0900

    refactor: 동영상 검색 함수 코드 수정

--- src/js/handlers/searchVideoControl.js
+++ src/js/handlers/searchVideoControl.js
@@ -1,49 +1,50 @@
-import {$} from '../utils/DOM.js';
-import {searchResult} from '../API.js';
-import {createSearchedVideoList} from '../templates/searchedVideoList.js';
-import {videoSkeletonTemplate} from '../templates/videoSkeleton.js';
-import {FETCH_VIDEO_COUNT} from '../constants/classroom.js';
-import {LATEST_KEYWORD} from '../constants/localStorage.js';
-import {searchNotFoundTemplate} from '../templates/searchNotFound.js';
-import {initInfiniteScroll} from './scrollControl.js';
-import {setLocalStorage} from '../utils/localStorage.js';
-import {pageToken} from '../states/pageToken.js';
-import {latestKeywords} from '../states/latestKeyword.js';
+import { $ } from "../utils/DOM.js";
+import { searchResult } from "../API.js";
+import { createSearchedVideoList } from "../templates/searchedVideoList.js";
+import { videoSkeletonTemplate } from "../templates/videoSkeleton.js";
+import { FETCH_VIDEO_COUNT } from "../constants/classroom.js";
+import { searchNotFoundTemplate } from "../templates/searchNotFound.js";
+import { initInfiniteScroll } from "./scrollControl.js";
+import { pageToken } from "../states/pageToken.js";
+import { latestKeywordList } from "../states/latestKeywordList.js";
+import { latestKeyword } from "../states/latestKeyword.js";
 
 export const searchVideoController = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const keyword = e.target.elements['video-search-input'].value;
-    onSearchKeyword(keyword);
+  const keyword = $("#video-search-input").value;
+  onSearchKeyword(keyword);
 };
 
-export const searchLatestKeywordController = ({target}) => {
-    if (!target.classList.contains('js-latest-keyword')) return;
-
+export const searchLatestKeywordController = ({ target }) => {
+  if (target.classList.contains("js-latest-keyword")) {
     const keyword = target.innerText;
-    $('#video-search-input').value = keyword;
+    $("#video-search-input").value = keyword;
     onSearchKeyword(keyword);
+  }
 };
 
 export const onSearchKeyword = async (keyword) => {
-    setLocalStorage(LATEST_KEYWORD, keyword);
-    latestKeywords.add(keyword);
+  latestKeyword.set(keyword);
+  latestKeywordList.add(keyword);
 
-    const items = await searchVideo(keyword);
-    if (!items.length) return;
-    initInfiniteScroll();
+  const results = await searchVideo(keyword);
+  results.length && initInfiniteScroll();
 };
 
 export const searchVideo = async (keyword) => {
-    const $videoSearchResult = $('#video-search-result');
-    $videoSearchResult.innerHTML = videoSkeletonTemplate().repeat(FETCH_VIDEO_COUNT);
+  const $videoSearchResult = $("#video-search-result");
+  $videoSearchResult.innerHTML =
+    videoSkeletonTemplate().repeat(FETCH_VIDEO_COUNT);
 
-    const {nextPageToken, items} = await searchResult(keyword);
+  const { nextPageToken, items } = await searchResult(keyword);
 
-    pageToken.set(nextPageToken);
-    $videoSearchResult.innerHTML = '';
+  pageToken.set(nextPageToken);
 
-    items.length ? createSearchedVideoList(items) : ($videoSearchResult.innerHTML = searchNotFoundTemplate());
+  $videoSearchResult.innerHTML = "";
+  items.length
+    ? createSearchedVideoList(items)
+    : ($videoSearchResult.innerHTML = searchNotFoundTemplate());
 
-    return items;
+  return items;
 };
