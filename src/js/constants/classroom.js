082a1ecc41ef9f08a8d281aa0c0954e7e996ff03
Yeewon <yes3427@kookmin.ac.kr>
2022-02-24 17:26:52 +0900

    feat: primary color 상수로 관리

--- src/js/constants/classroom.js
+++ src/js/constants/classroom.js
@@ -1,6 +1,8 @@
-export const SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-export const VIDEO_URL = 'https://www.googleapis.com/youtube/v3/videos';
+export const SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+export const VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos";
 
 export const FETCH_VIDEO_COUNT = 10;
 export const MAX_STORE_KEYWORD_COUNT = 3;
 export const MAX_SAVEABLE_VIDEO_COUNT = 100;
+
+export const PRIMARY_COLOR = "bg-cyan-100";
